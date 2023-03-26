f497773fb80bbc1e6405e18577f55f2499aba3bd
Olexandr Horbenko <horbenko.o.a@gmail.com>
2023-03-27 01:49:29 +0300

    Fix Axios

--- src/js/api/pixabayAPI.js
+++ src/js/api/pixabayAPI.js
@@ -1,22 +1,15 @@
-// js/api/pixabayAPI.js
+import axios from 'axios';
+
 export const API_KEY = '34746365-8c018e91f337134c616d4d454';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImagesFromApi(searchQuery, page) {
   try {
-    const response = await fetch(
-      `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-        searchQuery
-      )}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-    );
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+      searchQuery
+    )}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
 
-    if (!response.ok) {
-      throw new Error(
-        `An error occurred while fetching images: ${response.statusText}`
-      );
-    }
-
-    const { hits } = await response.json();
+    const { hits } = response.data;
     return hits;
   } catch (error) {
     console.error('Error:', error);
